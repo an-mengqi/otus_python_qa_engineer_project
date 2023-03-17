81ba5089f4e1372c69590bfa7fb6d7f032ca254d
Монахова Анастасия <a.klopova@dssl.ru>
2023-03-17 18:34:24 +0100

    tests work with selenoid and docker-compose

---
+++ conftest.py
@@ -0,0 +1,73 @@
+import allure
+import datetime
+import logging
+import os
+import pytest
+
+from selenium import webdriver
+
+
+@pytest.fixture
+def dog_breed_url():
+    dog_breed_url = "https://dog.ceo/api/breed/"
+    return dog_breed_url
+
+
+@pytest.fixture
+def dog_ceo_url():
+    dog_ceo_url = "https://dog.ceo"
+    return dog_ceo_url
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", default="chrome", help="choose browser: chrome, firefox, yandex")
+    parser.addoption("--driver_folder", default=os.path.expanduser("~/otus/drivers"))
+    parser.addoption("--url", action="store", default="http://192.168.0.15:8081")
+    parser.addoption("--log_level", action="store", default="DEBUG")
+
+    parser.addoption("--executor", action="store", default="192.168.0.15")
+    parser.addoption("--mobile", action="store_true")
+    parser.addoption("--vnc", action="store_true")
+    parser.addoption("--video", action="store_true")
+    parser.addoption("--bv")
+    parser.addoption("--logs", action="store_true")
+
+
+@pytest.fixture
+def browser(request):
+    _browser = request.config.getoption("--browser")
+    url = request.config.getoption("--url")
+    log_level = request.config.getoption("--log_level")
+    executor = request.config.getoption("--executor")
+    mobile = request.config.getoption("--mobile")
+    logs = request.config.getoption("--logs")
+
+    logger = logging.getLogger(request.node.name)
+    file_handler = logging.FileHandler(f"logs/{request.node.name}.log")
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(file_handler)
+    logger.setLevel(level=log_level)
+    logger.info("===> Test {} started at {}".format(request.node.name, datetime.datetime.now()))
+
+    executor_url = f"http://{executor}:4444/wd/hub"
+
+    caps = {
+        "browserName": _browser
+    }
+
+    driver = webdriver.Remote(command_executor=executor_url, desired_capabilities=caps)
+    if not mobile:
+        driver.maximize_window()
+
+    driver.log_level = log_level
+    driver.logger = logger
+    driver.test_name = request.node.name
+    driver.local_url = url
+    driver.admin_url = driver.local_url + "/admin"
+    driver.registration_url = driver.local_url + "/index.php?route=account/register"
+
+    def finalizer():
+        driver.quit()
+
+    request.addfinalizer(finalizer)
+    return driver
---
+++ tests/test_api.py
@@ -0,0 +1,41 @@
+import pytest
+import requests
+
+
+def test_send_existing_url(dog_ceo_url):
+    """Check that status code of a specific existing url is 200"""
+    response = requests.get(dog_ceo_url + "/dog-api")
+    assert response.status_code == 200
+
+
+def test_send_nonexistent_url(dog_ceo_url):
+    """Check that status code of a nonexistent url is 404"""
+    response = requests.get(dog_ceo_url + "/nonexistent-url")
+    assert response.status_code == 404
+
+
+@pytest.mark.parametrize("breed", ["dachshund",
+                                   "samoyed",
+                                   "pug"])
+def test_check_breed(dog_breed_url, breed):
+    """Check that there are specific breeds in response url message"""
+    response = requests.get(dog_breed_url + breed + "/images/random")
+    assert breed in response.json()["message"]
+
+
+@pytest.mark.parametrize("breed", ["hound",
+                                   "mastiff"])
+def test_check_subbreed(dog_breed_url, breed):
+    """Check that there are sub-breeds of specific breeds (hound, mastiff) in
+    response url message"""
+    subbreed = requests.get(dog_breed_url + breed + "/list")
+    response = requests.get(dog_breed_url + breed + "/images/random")
+    for x in subbreed.json()["message"]:
+        if x in response.json()["message"]:
+            assert breed + "-" + x in response.json()["message"]
+
+
+def test_check_image_status(dog_ceo_url):
+    """Check that status (in json) of a random image is Success"""
+    response = requests.get(dog_ceo_url + "/api/breeds/image/random")
+    assert "success" in response.json()["status"]
