5e66d26f8b67efb7c1c0b8fe9d37d7b0ab8b76cc
Монахова Анастасия <a.klopova@dssl.ru>
2023-03-19 13:06:51 +0100

    allure added

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,19 +1,23 @@
+import allure
+
 import pytest
 import requests
 
-
+@allure.step("Проверяю статус код существующего url")
 def test_send_existing_url(dog_ceo_url):
     """Check that status code of a specific existing url is 200"""
     response = requests.get(dog_ceo_url + "/dog-api")
     assert response.status_code == 200
 
 
+@allure.step("Проверяю статус код несуществующего url")
 def test_send_nonexistent_url(dog_ceo_url):
     """Check that status code of a nonexistent url is 404"""
     response = requests.get(dog_ceo_url + "/nonexistent-url")
     assert response.status_code == 404
 
 
+@allure.step("Проверяю наличие пород в message в ответе json")
 @pytest.mark.parametrize("breed", ["dachshund",
                                    "samoyed",
                                    "pug"])
@@ -23,6 +27,7 @@ def test_check_breed(dog_breed_url, breed):
     assert breed in response.json()["message"]
 
 
+@allure.step("Проверяю наличие подпород в message в ответе json")
 @pytest.mark.parametrize("breed", ["hound",
                                    "mastiff"])
 def test_check_subbreed(dog_breed_url, breed):
@@ -35,6 +40,7 @@ def test_check_subbreed(dog_breed_url, breed):
             assert breed + "-" + x in response.json()["message"]
 
 
+@allure.step("Проверяю статус код в ответе json рандомной породы")
 def test_check_image_status(dog_ceo_url):
     """Check that status (in json) of a random image is Success"""
     response = requests.get(dog_ceo_url + "/api/breeds/image/random")
